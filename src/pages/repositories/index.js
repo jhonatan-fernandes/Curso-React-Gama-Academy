3ce68f16d1b7da7ed1917364e492f3f06f4d55ce
Jhonatan Fernandes <jhonnfernandes90@gmail.com>
2021-07-25 17:59:03 -0300

    Adicionando arquivos para o git

---
+++ src/pages/repositories/index.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
+
+
+export default function Repositories ( ) {
+    const [repositories , setRepositories ] = useState([ ]);
+    const history = useHistory( );
+
+    useEffect( ( ) => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        
+        if (repositoriesName != null) {
+        repositoriesName = JSON.parse(repositoriesName);
+        console.log(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear( );
+        } else {
+            history.push('/');
+        }
+    }, [ ] );
+
+    return (
+        <S.Container>
+        <S.Title>Repositórios</S.Title>
+        <S.List>
+            {repositories.map(repository => {
+                return (
+                    <S.ListItem> Repositorio: {repository }</S.ListItem>
+            )
+            }) }
+        </S.List>
+
+        <S.LinkHome to= "/">Voltar</S.LinkHome>
+        </S.Container>
+    )
+}
